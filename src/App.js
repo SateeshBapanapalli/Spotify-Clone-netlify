0220c5c8c2666fbc47cde121849c5005eff98fc2
SateeshBapanapalli <sateeshb1997@gmail.com>
2023-03-04 15:25:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import PlayerPlaylist from './components/PlayerPlaylist';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      
+      <div className='player-container'>
+        <div className='side-bar'>
+          <Sidebar/>
+        </div>
+        <div className='player-playlist'>
+          <PlayerPlaylist/>
+        </div>
+      </div>
+
+      <div className='footer'>
+        <Footer/>
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
